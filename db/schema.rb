8bb9ea8b9230127dc7ca3592e80c77d23cd9930b
MD ARAFAT HOSSAIN <arafat.kd99@gmail.com>
2025-07-04 03:55:49 +0600

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,4 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_05_190003) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "specifications", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
